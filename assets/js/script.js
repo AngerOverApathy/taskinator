35f90ece53c8a1f02fe638fc029c6da2b74af134
AngerOverApathy <violasian@gmail.com>
2022-09-07 21:25:39 -0700

    appending tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,18 @@ let tasksToDoEl = document.querySelector('#tasks-to-do')
 
 let createTaskHandler = function(event) {
     event.preventDefault()
-    
+
+    let taskTypeInput = document.querySelector("select[name='task-type']").value
+let taskNameInput = document.querySelector('input[name="task-name"]').value
+
     let lisItemEl = document.createElement('li')
     lisItemEl.className = 'task-item'
-    lisItemEl.textContent = 'This is a new task'
+
+    let taskInfoEl = document.createElement('div')
+    taskInfoEl.className = 'task-info'
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+
+    lisItemEl.appendChild(taskInfoEl)
     tasksToDoEl.appendChild(lisItemEl)
 }
 
